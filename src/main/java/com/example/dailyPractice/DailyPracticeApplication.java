f9d4adb4e060fae92816a9e3cbf7e80da2e32595
feixuanyu <johnny.fei@lv.com>
2024-05-16 15:57:18 +0800

    init

---
+++ src/main/java/com/example/dailyPractice/DailyPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dailyPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dailyPractice/DailyPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailyPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
