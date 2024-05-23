72d82fb67a4b2ff80c3ab65ddcfe5d6f5ea8c129
masonFei <994085848@qq.com>
2024-05-23 23:07:01 +0800

    找出最长等值子数组-滑动窗口、哈希表

--- src/main/java/com/example/dailyPractice/LongestEqualSubarray.java
+++ src/main/java/com/example/dailyPractice/LongestEqualSubarray.java
@@ -24,14 +24,16 @@ public class LongestEqualSubarray {
 //    期望结果:3
 
     public static void main(String[] args) {
-        int[] intArray = {3,1,5,3,1,1};
+        int[] intArray = {9, 5, 1, 4, 2, 3, 4, 13, 5, 2, 7, 2, 2, 14};
+//        int[] intArray = {3,1,5,3,1,1};
 //        int[] intArray = {1};
 //        int[] intArray = {3,4,3,5,3,6,3,8,3,8,3,8,8};
 //        int[] intArray = {1, 3, 2, 3, 1, 3};
 //        int[] intArray = {1, 1, 2, 2, 1, 1};
         List<Integer> nums = IntStream.of(intArray).boxed().collect(Collectors.toList());
 //        int k = 2;
-        int k = 0;
+//        int k = 0;
+        int k = 4;
         int res = longestEqualSubarray(nums, k);
         System.out.println(res);
     }
@@ -45,63 +47,21 @@ public class LongestEqualSubarray {
 
 
     public static int longestEqualSubarray(List<Integer> nums, int k) {
-        Map<Integer, LinkedList<Integer>> numsItemRangeMap = new HashMap<>();
-
-        // 统计下不同的数字在数组中出现的位置
-        for (int i = 0; i < nums.size(); i++) {
-            Integer number = nums.get(i);
-            LinkedList<Integer> indexList = new LinkedList<>();
-            if (numsItemRangeMap.containsKey(number)) {
-                indexList = numsItemRangeMap.get(number);
-            }
-            indexList.add(i);
-            numsItemRangeMap.put(number, indexList);
+        int n = nums.size();
+        Map<Integer, List<Integer>> pos = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            pos.computeIfAbsent(nums.get(i), x -> new ArrayList<>()).add(i);
         }
-        // 对map中存在重复数字的数组，双指针计算其下标之间的距离
-        Map<Integer, Integer> numColledMap = new HashMap<>();
-        int maxColledNum = nums.isEmpty() ? 0 : 1;
-
-        for (Map.Entry<Integer, LinkedList<Integer>> entry : numsItemRangeMap.entrySet()) {
-            Integer numberkey = entry.getKey();
-            LinkedList<Integer> indexList = entry.getValue();
-            if (indexList.size() > 1) {
-                int left = 0;
-                int right = 1;
-                int cost = 0;
-                int colledNum = 1;
-                numColledMap.put(numberkey, colledNum);
-                while (right < indexList.size()) {
-                    cost += indexList.get(right) - indexList.get(left) - 1;
-
-                    if (cost > k) {
-                        left = right;
-                        right++;
-                        if (colledNum > numColledMap.get(numberkey)) {
-                            numColledMap.put(numberkey, colledNum);
-                        }
-                        colledNum = 0;
-                        cost = 0;
-                    } else {
-                        left++;
-                        right++;
-                    }
-
-                    colledNum++;
-
-                    if (right == indexList.size()) {
-                        if (colledNum > numColledMap.get(numberkey)) {
-                            numColledMap.put(numberkey, colledNum);
-                        }
-                    }
-
+        int ans = 0;
+        for (List<Integer> vec : pos.values()) {
+            for (int i = 0, j = 0; i < vec.size(); i++) { /* 缩小窗口，直到不同元素数量小于等于 k */
+                while (vec.get(i) - vec.get(j) - (i - j) > k) {
+                    j++;
                 }
-
-                if (numColledMap.get(numberkey) > maxColledNum) {
-                    maxColledNum = numColledMap.get(numberkey);
-                }
+                ans = Math.max(ans, i - j + 1);
             }
         }
-        return maxColledNum;
+        return ans;
 
     }
 
