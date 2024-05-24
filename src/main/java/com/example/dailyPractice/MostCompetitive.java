21800843c73c53c4a3fc143a7efa6046b84083c5
feixuanyu <johnny.fei@lv.com>
2024-05-24 14:03:44 +0800

    最有竞争力的数组，代码暂存

---
+++ src/main/java/com/example/dailyPractice/MostCompetitive.java
@@ -0,0 +1,48 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2024 All Rights Reserved.
+ */
+package com.example.dailyPractice;
+
+/**
+ * MostCompetitive
+ *
+ * @author johnny.fei
+ * @version 1.0.0
+ * @since 2024-05-24 13:32
+ */
+public class MostCompetitive {
+
+    //输入：nums = [3,5,2,6], k = 2
+    //输出：[2,6]
+
+    //输入：nums = [2,4,3,3,5,4,9,6], k = 4
+    //输出：[2,3,3,4]
+
+    //输入：nums = [2,3,5,2,6], k = 2
+    //输出：[2,2]
+
+    //输入：nums = [2,3,5,2,6,1], k = 2
+    //输出：[2,1]
+
+    //输入：nums = [2,4,3,3,5,4,9,6,1], k = 4
+    //输出：[2,3,3,1]
+
+    //输入：nums = [2,4,1,3,3,5,1,9,6,1], k = 4
+    //输出：[1,3,3,1]
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public int[] mostCompetitive(int[] nums, int k) {
+
+        int[] res = new int[k];
+
+        return res;
+
+    }
+
+
+}
