b5ab9d60373ddb52dcbde6cc2b62b9c60771b426
masonFei <994085848@qq.com>
2024-05-28 22:11:07 +0800

    LCR193-二叉搜索树的最近公共祖先 用好二叉搜索树，同时直接将找到的结果在左右分支输出

--- src/main/java/com/example/dailyPractice/LowestCommonAncestor.java
+++ src/main/java/com/example/dailyPractice/LowestCommonAncestor.java
@@ -47,48 +47,16 @@ public class LowestCommonAncestor {
 //
 //    百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。
     public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        frontSort(root, p, q);
 
-        return resNode;
-    }
-
-    public static int frontSort(TreeNode root, TreeNode p, TreeNode q) {
-
-        int leftRes = 0;
-        if (root.left != null) {
-            leftRes = frontSort(root.left, p, q);
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
         }
 
-        int rightRes = 0;
-        if (root.right != null) {
-            rightRes = frontSort(root.right, p, q);
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
         }
 
-        if (leftRes == 3 || rightRes == 3) {
-            return 3;
-        }
-
-
-        if (leftRes + rightRes == 3 || ((leftRes == 1 || rightRes == 1) && root.val == q.val) || ((leftRes == 2 || rightRes == 2) && root.val == p.val)) {
-            resNode = root;
-            return 3;
-        } else {
-
-            if (leftRes != 0) {
-                return leftRes;
-            } else if (rightRes != 0) {
-                return rightRes;
-            } else {
-                if (root.val == p.val) {
-                    return 1;
-                } else if (root.val == q.val) {
-                    return 2;
-                } else {
-                    return 0;
-                }
-            }
-        }
-
+        return root;
     }
 
 
