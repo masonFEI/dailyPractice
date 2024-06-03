2061319a876a53ae2eb11d958afb64543cb268ac
masonFei <994085848@qq.com>
2024-06-03 23:55:05 +0800

    LCR 194. 二叉树的最近公共祖先 优化递归

--- src/main/java/com/example/dailyPractice/LowestCommonAncestorII.java
+++ src/main/java/com/example/dailyPractice/LowestCommonAncestorII.java
@@ -30,8 +30,20 @@ public class LowestCommonAncestorII {
 
 
     public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        backSort(root, p, q);
-        return resNode;
+
+        if (root == null || root.val == p.val || root.val == q.val) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else return right;
+
     }
 
     public static int backSort(TreeNode root, TreeNode p, TreeNode q) {
