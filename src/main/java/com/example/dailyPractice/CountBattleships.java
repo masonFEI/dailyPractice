24924c7f7087b1cf732644ce3b2eb6cfb1be72a1
masonFei <994085848@qq.com>
2024-06-11 22:15:14 +0800

    419 甲板上的战舰

--- src/main/java/com/example/dailyPractice/CountBattleships.java
+++ src/main/java/com/example/dailyPractice/CountBattleships.java
@@ -14,46 +14,24 @@ package com.example.dailyPractice;
 public class CountBattleships {
 
 
-    public static int shipCount;
 
-    // 查找连续的方向为右，下，左，上
+//    具体来说，如果位于 (i,j) 的格子是战舰的头部，那么左边和上边的相邻格子不能是 X，即：
+//
+//    如果 j>0，那么 (i,j-1) 不能是 X。
+//    如果 i>0，那么 (i−1,j) 不能是 X
+
     public int countBattleships(char[][] board) {
-
-        shipCount = 0;
-        int[][] memo = new int[board.length][board[0].length];
-
+        int ans = 0;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-
+                if (board[i][j] == 'X' &&
+                        (j == 0 || board[i][j - 1] != 'X') &&
+                        (i == 0 || board[i - 1][j] != 'X')) {
+                    ans++;
+                }
             }
         }
-
-        return shipCount;
-    }
-
-
-    public void deepSearch(int i, int j, int direction, int[][] memo, char[][] board) {
-        if (memo[i][j] != 0) {
-            return;
-        }
-
-        // 是X，则往同一个方向搜索
-        if (String.valueOf(board[i][j]).equals("X")) {
-            memo[i][j] = 1;
-            // 向右
-            if (direction == 0 && i + 1 < board[0].length) {
-
-            } else if (direction == 1 && j + 1 < board.length) {
-                // 向下
-            } else if (direction == 2 && i - 1 >= 0) {
-                // 向左
-            } else if (direction == 2 && j - 1 >= 0) {
-                // 向上
-            }
-
-        }
-
-
+        return ans;
     }
 
 
