91cda62e9814df838e822c41f10bd6fe48275616
feixuanyu <994085848@qq.com>
2024-06-14 14:17:40 +0800

    2786 访问数组中的位置使分数最大

---
+++ src/main/java/com/example/dailyPractice/MaxScore.java
@@ -0,0 +1,88 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2024 All Rights Reserved.
+ */
+package com.example.dailyPractice;
+
+/**
+ * MaxScore
+ *
+ * @author johnny.fei
+ * @version 1.0.0
+ * @since 2024-06-14 11:09
+ */
+public class MaxScore {
+
+    public static void main(String[] args) {
+//[2,3,6,1,9,2]
+//        5
+
+        int[] nums = {2, 3, 6, 1, 9, 2};
+        int x = 5;
+
+        long res = maxScore(nums, x);
+        System.out.println(res);
+    }
+
+    public static long maxScore(int[] nums, int x) {
+
+        int[][] dp = new int[nums.length][4];
+        // 加当前值的余数
+        dp[0][0] = nums[0] % 2;
+        // 加当前值的和
+        dp[0][1] = nums[0];
+        // 不加当前值的余数
+        dp[0][2] = nums[0] % 2;
+        // 不加当前值的和
+        dp[0][3] = nums[0];
+
+        // 加当前值的余数
+        dp[1][0] = nums[1] % 2;
+        int tmpSum1 = nums[0];
+        if (dp[1][0] != dp[0][0]) {
+            tmpSum1 += nums[1] - x;
+        } else {
+            tmpSum1 += nums[1];
+        }
+
+        // 和
+        dp[1][1] = tmpSum1;
+        // 不加当前值 的 余数
+        dp[1][2] = nums[0] % 2;
+        // 不加当前值 的 余数
+        dp[1][3] = nums[0];
+
+        for (int i = 2; i < nums.length; i++) {
+            int yuItem = nums[i] % 2;
+            // 加前一个数后的余数
+            int preYu1 = dp[i - 1][0];
+            // 加前一个数后的和
+            int tmpSumPre1 = dp[i - 1][1];
+            // 不加前一个数后的余数
+            int preYu2 = dp[i - 1][2];
+            // 不加前一个数后的和
+            int tmpSumPre2 = dp[i - 1][3];
+
+            if (yuItem != preYu1) {
+                tmpSumPre1 += nums[i] - x;
+            } else {
+                tmpSumPre1 += nums[i];
+            }
+
+            if (yuItem != preYu2) {
+                tmpSumPre2 += nums[i] - x;
+            } else {
+                tmpSumPre2 += nums[i];
+            }
+            dp[i][0] = yuItem;
+            dp[i][1] = Math.max(tmpSumPre1, tmpSumPre2);
+
+            // 不加当前数
+            dp[i][2] = dp[i - 1][1] > dp[i - 1][3] ? dp[i - 1][0] : dp[i - 1][2];
+            dp[i][3] = Math.max(dp[i - 1][1], dp[i - 1][3]);
+        }
+
+        return Math.max(dp[nums.length - 1][1], dp[nums.length - 1][3]);
+    }
+
+}
