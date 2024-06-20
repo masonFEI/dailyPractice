1cf23db7d6075f1e1dd6944dea88db2a91c0c3a0
masonFei <994085848@qq.com>
2024-06-20 22:13:53 +0800

    2748. 美丽下标对的数目

---
+++ src/main/java/com/example/dailyPractice/CountBeautifulPairs.java
@@ -0,0 +1,27 @@
+package com.example.dailyPractice;
+
+public class CountBeautifulPairs {
+
+
+    public int countBeautifulPairs(int[] nums) {
+        int ans = 0;
+        int[] cnt = new int[10];
+        for (int x : nums) {
+            for (int y = 1; y < 10; y++) {
+                if (cnt[y] > 0 && gcd(y, x % 10) == 1) {
+                    ans += cnt[y];
+                }
+            }
+            while (x >= 10) {
+                x /= 10;
+            }
+            cnt[x]++; // 统计最高位的出现次数
+        }
+        return ans;
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+}
