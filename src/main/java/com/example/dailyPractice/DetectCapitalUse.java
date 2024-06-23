0e898d31e872438f17e406f8ee783c35b3602823
masonFei <994085848@qq.com>
2024-06-23 15:15:22 +0800

    520. 检测大写字母

---
+++ src/main/java/com/example/dailyPractice/DetectCapitalUse.java
@@ -0,0 +1,20 @@
+package com.example.dailyPractice;
+
+public class DetectCapitalUse {
+
+
+    public boolean detectCapitalUse(String word) {
+
+        int cnt = 0;
+        for (char c : word.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                cnt++;
+            }
+        }
+
+        return cnt == 0 || cnt == word.length() || (cnt == 1 && Character.isUpperCase(word.charAt(0)));
+
+    }
+
+
+}
