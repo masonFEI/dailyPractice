c24da74e2a7e3d654395ad2731ef9516abbd76bd
masonFei <994085848@qq.com>
2024-07-11 21:29:14 +0800

    2972. 统计移除递增子数组的数目 II

---
+++ src/main/java/com/example/dailyPractice/IncremovableSubarrayCountII.java
@@ -0,0 +1,30 @@
+package com.example.dailyPractice;
+
+public class IncremovableSubarrayCountII {
+
+
+
+    public long incremovableSubarrayCount(int[] a) {
+        int n = a.length;
+        int i = 0;
+        while (i < n - 1 && a[i] < a[i + 1]) {
+            i++;
+        }
+        if (i == n - 1) { // 每个非空子数组都可以移除
+            return (long) n * (n + 1) / 2;
+        }
+
+        long ans = i + 2; // 不保留后缀的情况，一共 i+2 个
+        // 枚举保留的后缀为 a[j:]
+        for (int j = n - 1; j == n - 1 || a[j] < a[j + 1]; j--) {
+            while (i >= 0 && a[i] >= a[j]) {
+                i--;
+            }
+            // 可以保留前缀 a[:i+1], a[:i], ..., a[:0] 一共 i+2 个
+            ans += i + 2;
+        }
+        return ans;
+    }
+
+
+}
