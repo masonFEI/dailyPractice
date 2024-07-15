2df9329351d795870d0d8ae06a075897e4a65bdf
masonFei <994085848@qq.com>
2024-07-15 22:15:36 +0800

    721. 账户合并

--- src/main/java/com/example/dailyPractice/AccountsMerge.java
+++ src/main/java/com/example/dailyPractice/AccountsMerge.java
@@ -5,6 +5,7 @@
 package com.example.dailyPractice;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * AccountsMerge
@@ -17,47 +18,64 @@ public class AccountsMerge {
 
 
     public static void main(String[] args) {
+//        String[][] accountsArray = {{"John","johnsmith@mail.com","john_newyork@mail.com"},
+//        {"John","johnsmith@mail.com","john00@mail.com"}, {"Mary","mary@mail.com"},
+//        {"John","johnnybravo@mail.com"}};
+        String[][] accountsArray = {{"Gabe","Gabe0@m.co","Gabe3@m.co","Gabe1@m.co"},
+                {"Kevin","Kevin3@m.co","Kevin5@m.co","Kevin0@m.co"}, {"Ethan","Ethan5@m.co","Ethan4@m.co","Ethan0@m.co"},
+                {"Hanzo","Hanzo3@m.co","Hanzo1@m.co","Hanzo0@m.co"},{"Fern","Fern5@m.co","Fern1@m.co","Fern0@m.co"}};
 
+        List<List<String>> accountsList = Arrays.stream(accountsArray)
+                .map(Arrays::stream)  // 将每个子数组转换为Stream
+                .map(stream -> stream.collect(Collectors.toList()))  // 将每个Stream收集为List
+                .collect(Collectors.toList());
+
+        List<List<String>> res = accountsMerge(accountsList);
+        System.out.println(res);
     }
 
 
-    public List<List<String>> accountsMerge(List<List<String>> accounts) {
-
-        Map<String, List<String>> emailsMap = new HashMap<String, List<String>>();
+    public static List<List<String>> accountsMerge(List<List<String>> accounts) {
+        Map<String, List<Integer>> emailToIdx = new HashMap<>();
         for (int i = 0; i < accounts.size(); i++) {
-            List<String> item = accounts.get(i);
-            for (int j = 1; j < item.size(); j++) {
-                if (emailsMap.containsKey(item.get(i))) {
-                    List<String> allData = new ArrayList<>();
-                    List<String> emails = emailsMap.get(item.get(i));
-                    List<String> name = emails.subList(0, 1);
-                    List<String> oldEmails = emails.subList(1, emails.size());
-                    List<String> newEmails = item.subList(1, item.size());
-                    oldEmails.addAll(newEmails);
-                    Collections.sort(oldEmails);
-                    allData.addAll(name);
-                    allData.addAll(oldEmails);
+            for (int k = 1; k < accounts.get(i).size(); k++) {
+                emailToIdx.computeIfAbsent(accounts.get(i).get(k), x -> new ArrayList<>()).add(i);
+            }
+        }
 
-                    for (int k = 1; k < allData.size(); k++) {
-                        emailsMap.put(allData.get(k), allData);
-                    }
-                    break;
-                } else {
-                    emailsMap.put(item.get(i), item);
+        List<List<String>> ans = new ArrayList<>();
+        boolean[] vis = new boolean[accounts.size()];
+        Set<String> emailSet = new HashSet<>(); // 用于收集 DFS 中访问到的邮箱地址
+        for (int i = 0; i < accounts.size(); i++) {
+            if (vis[i]) {
+                continue;
+            }
+            emailSet.clear();
+            dfs(i, accounts, emailToIdx, vis, emailSet);
+
+            List<String> res = new ArrayList<>(emailSet);
+            Collections.sort(res);
+            res.add(0, accounts.get(i).get(0));
+
+            ans.add(res);
+        }
+        return ans;
+    }
+
+    private static void dfs(int i, List<List<String>> accounts, Map<String, List<Integer>> emailToIdx, boolean[] vis, Set<String> emailSet) {
+        vis[i] = true;
+        for (int k = 1; k < accounts.get(i).size(); k++) {
+            String email = accounts.get(i).get(k);
+            if (emailSet.contains(email)) {
+                continue;
+            }
+            emailSet.add(email);
+            for (int j : emailToIdx.get(email)) { // 遍历所有包含该邮箱地址的账户下标 j
+                if (!vis[j]) { // j 没有访问过
+                    dfs(j, accounts, emailToIdx, vis, emailSet);
                 }
             }
-
         }
-
-        List<List<String>> resList = new ArrayList<>();
-
-        for (List<String> emailList : emailsMap.values()) {
-            resList.add(new ArrayList<>(emailList));
-        }
-
-        return resList;
-
-
     }
 
 }
