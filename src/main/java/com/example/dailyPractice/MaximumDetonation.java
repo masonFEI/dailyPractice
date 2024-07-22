edb20dcbe99fe39d02eef2dd92bef262d73596c7
masonFei <994085848@qq.com>
2024-07-22 21:52:46 +0800

    2101. 引爆最多的炸弹

--- src/main/java/com/example/dailyPractice/MaximumDetonation.java
+++ src/main/java/com/example/dailyPractice/MaximumDetonation.java
@@ -18,13 +18,13 @@ public class MaximumDetonation {
 
     public static void main(String[] args) {
 //        int[][] bombs = {{2, 1, 3}, {6, 1, 4}};
-        int[][] bombs = {{1,1,100000}, {100000,100000,1}};
+//        int[][] bombs = {{1, 1, 100000}, {100000, 100000, 1}};
 
 //        int[][] bombs = {{1,1,5}, {10,10,5}};
 //        int[][] bombs = {{56,80,2},{55,9,10},{32,75,2},{87,89,1},{61,94,3},{43,82,9},{17,100,6},{50,6,7},{9,66,7},{98,3,6},{67,50,2},{79,39,5},{92,60,10},{49,9,9},{42,32,10}};
 //        int[][] bombs = {{54, 95, 4}, {99, 46, 3}, {29, 21, 3}, {96, 72, 8}, {49, 43, 3}, {11, 20, 3}, {2, 57, 1}, {69, 51, 7}, {97, 1, 10}, {85, 45, 2}, {38, 47, 1}, {83, 75, 3}, {65, 59, 3}, {33, 4, 1}, {32, 10, 2}, {20, 97, 8}, {35, 37, 3}};
 
-//        int[][] bombs = {{1, 2, 3}, {2, 3, 1}, {3, 4, 2}, {4, 5, 3}, {5, 6, 4}};
+        int[][] bombs = {{1, 2, 3}, {2, 3, 1}, {3, 4, 2}, {4, 5, 3}, {5, 6, 4}};
 
         int res = maximumDetonation(bombs);
         System.out.println(res);
@@ -44,42 +44,34 @@ public class MaximumDetonation {
 
 
     public static int maximumDetonation(int[][] bombs) {
-
+        int n = bombs.length;
         // 1.先找出来每个炸弹直接关联引爆的炸弹
         // 2.根据关联数组，计算每个点可以到达的节点的数量（深搜）
-        Map<Integer, List<Integer>> bombMap = new HashMap<>();
+        List<Integer>[] g = new ArrayList[n];
+        Arrays.setAll(g, i -> new ArrayList<>());
         for (int i = 0; i < bombs.length; i++) {
             int[] bombItem = bombs[i];
-            int index = 0;
-            while (index < bombs.length) {
-                if (index != i) {
-                    int[] bombThat = bombs[index];
 
-                    int xDistance = Math.abs(bombItem[0] - bombThat[0]);
-                    long xSquare = (long) xDistance * xDistance;
+            for (int j = 0; j < bombs.length; j++) {
+                int[] bombThat = bombs[j];
 
-                    int yDistance = Math.abs(bombItem[1] - bombThat[1]);
-                    long ySquare = (long) yDistance * yDistance;
+                int xDistance = Math.abs(bombItem[0] - bombThat[0]);
+                long xSquare = (long) xDistance * xDistance;
 
-                    // 距离过远，不会引爆，要在第一个炸弹的爆炸范围内
-                    if (xSquare + ySquare <= (long) bombItem[2] * bombItem[2]) {
-                        List<Integer> bombed = new ArrayList<>();
-                        if (bombMap.containsKey(i)) {
-                            bombed = bombMap.get(i);
-                        }
-                        bombed.add(index);
-                        bombMap.put(i, bombed);
-                    }
+                int yDistance = Math.abs(bombItem[1] - bombThat[1]);
+                long ySquare = (long) yDistance * yDistance;
+
+                // 距离过远，不会引爆，要在第一个炸弹的爆炸范围内
+                if (xSquare + ySquare <= (long) bombItem[2] * bombItem[2]) {
+                    g[i].add(j);
                 }
-                index++;
             }
         }
         int maxBomb = 1;
-
-        Iterator<Map.Entry<Integer, List<Integer>>> iterator = bombMap.entrySet().iterator();
-        while (iterator.hasNext()) {
-            Map.Entry<Integer, List<Integer>> dsfParam = iterator.next();
-            int dsfCount = dsf(bombMap, new HashMap<>(), new HashMap<>(), dsfParam.getKey());
+        boolean[] checked = new boolean[n];
+        for (int i = 0; i < g.length; i++) {
+            Arrays.fill(checked, false);
+            int dsfCount = dsf(g, checked, i);
             maxBomb = Math.max(maxBomb, dsfCount);
         }
 
@@ -88,29 +80,16 @@ public class MaximumDetonation {
     }
 
 
-    private static int dsf(Map<Integer, List<Integer>> bombMap, Map<Integer, Integer> bombedMap, Map<Integer, Integer> checkedBombMap, Integer nowBomb) {
-        checkedBombMap.put(nowBomb, 1);
-        List<Integer> nowBombChildren = bombMap.get(nowBomb);
+    private static int dsf(List<Integer>[] g, boolean[] checked, Integer y) {
 
-        if (nowBombChildren == null || nowBombChildren.isEmpty()) {
-            if (!bombedMap.containsKey(nowBomb)) {
-                bombedMap.put(nowBomb, 1);
-                return 1;
-            } else {
-                return 0;
+        checked[y] = true;
+
+        int bombsCount = 1;
+
+        for (int i = 0; i < g[y].size(); i++) {
+            if (!checked[g[y].get(i)]) {
+                bombsCount += dsf(g, checked, g[y].get(i));
             }
-        }
-
-        int bombsCount = 0;
-        for (int i = 0; i < nowBombChildren.size(); i++) {
-            if (!checkedBombMap.containsKey(nowBombChildren.get(i))) {
-                bombsCount += dsf(bombMap, bombedMap, checkedBombMap, nowBombChildren.get(i));
-            }
-        }
-
-        if (!bombedMap.containsKey(nowBomb)) {
-            bombedMap.put(nowBomb, 1);
-            bombsCount += 1;
         }
 
         return bombsCount;
