189bc71da3442845fdcc1d1295f510fbfef0f91c
feixuanyu <994085848@qq.com>
2024-07-30 20:32:17 +0800

    2961 双模幂运算 代码暂存
    [[30,5,43,2],[15,50,35,41],[45,34,41,32],[14,37,33,13],[6,8,1,53],[37,1,12,52],[42,37,2,52],[9,2,15,3],[31,12,21,24],[52,24,6,12],[51,35,21,52],[30,18,10,2],[27,31,50,27],[29,25,26,32],[15,38,43,17],[22,12,16,43],[48,9,15,6],[41,26,22,21],[41,49,52,26],[53,38,9,33]]
    			1

--- src/main/java/com/example/dailyPractice/GetGoodIndices.java
+++ src/main/java/com/example/dailyPractice/GetGoodIndices.java
@@ -4,6 +4,8 @@
  */
 package com.example.dailyPractice;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,24 +24,38 @@ public class GetGoodIndices {
 //        int[][] variables = {{2,3,3,10},{3,3,3,1},{6,1,1,4}};
 //        int target = 2;
 
-        int[][]  variables = {{39,3,1000,1000}};
-        int target = 17;
+//        int[][]  variables = {{39,3,1000,1000}};
+//        int target = 17;
+
+        int[][] variables = {{31, 12, 21, 24}};
+        int target = 1;
 
         List<Integer> res = getGoodIndices(variables, target);
         System.out.println(res);
     }
 
 
-
     public static List<Integer> getGoodIndices(int[][] variables, int target) {
 
         List<Integer> res = new ArrayList<>();
+        MathContext mc = new MathContext(0);
         for (int i = 0; i < variables.length; i++) {
-            double value = Math.pow(Math.pow(variables[i][0], variables[i][1]) % 10, variables[i][2]) % variables[i][3];
-            if (value == target) {
+            int[] variable = variables[i];
+            BigDecimal base = new BigDecimal(variable[0]);
+            BigDecimal exponent = new BigDecimal(variable[1]);
+            // 使用 BigDecimal 的 pow 方法计算幂次方
+            BigDecimal result = base.pow(exponent.intValueExact(), mc);
+            BigDecimal divisor = new BigDecimal("10"); // 你的除数
+            BigDecimal base1 = result.remainder(divisor);
+            BigDecimal exponent1 = new BigDecimal(variable[2]);
+            BigDecimal result1 = base1.pow(exponent1.intValueExact(), mc);
+
+            BigDecimal divisor1 = new BigDecimal(variable[3]); // 你的除数
+            BigDecimal finalRes = result1.remainder(divisor1);
+
+            if (finalRes.compareTo(new BigDecimal(target)) == 0) {
                 res.add(i);
             }
-
         }
 
         return res;
